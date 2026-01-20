4940054263d68f51b30bc84448412d03e35f02c9
Nguyen Anh Duc <anhduc20053110@gmail.com>
2026-01-21 02:39:50 +0700

    feat: split to module

---
+++ my_deep_lib/activations.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
